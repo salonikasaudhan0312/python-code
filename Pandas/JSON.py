920efc5c98a47bd80210422da9fe30ac73844e0f
Saloni <saloni.kasaudhan0312@gmail.com>
2026-09-21 22:33:05 +0530

    JSON

--- Pandas/JSON.py
+++ Pandas/JSON.py
@@ -1,4 +1,5 @@
-from turtle import pd
-df=pd.read_json(r"C:\Users\HP\OneDrive\Desktop\python\Pandas\airports.json")
-f=pd.DataFrame(df)
+import pandas as pd
+import numpy as np
+df = pd.read_json(r"C:\Users\HP\OneDrive\Desktop\python\Pandas\airports.json")
+f = pd.DataFrame(df)
 print(f)
