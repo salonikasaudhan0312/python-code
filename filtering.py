177aee7ae49206066473c53a1b9569e41c169528
Saloni <saloni.kasaudhan0312@gmail.com>
2026-09-23 23:26:05 +0530

    filter

--- filtering.py
+++ filtering.py
@@ -3,4 +3,9 @@ import pandas as pd
 df = pd.read_csv(r"C:\Users\HP\Downloads\archive.zip")
 df = df[df["aqi"]>100]
 df = df[(df["aqi"]>100) & (df["temperature"]> 30)]
+aqi_data = df[(df["aqi"] >100) & (df["temperature"]>30 )] [ ["city", "aqi"]]
+print(aqi_data)
+print(aqi_data.iloc[0])
+print(aqi_data.loc[6])
+
 print(df)
